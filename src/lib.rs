2018de1f7d65c7a5f1b5b344ce69e98e1c7cb060
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-08-19 23:02:52 +0100

    create a basic crate template

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#![doc = include_str!("../README.md")]
+#![warn(
+	clippy::all,
+	clippy::pedantic,
+	clippy::correctness,
+	clippy::perf,
+	clippy::style,
+	clippy::suspicious,
+	clippy::complexity,
+	clippy::nursery,
+	clippy::unwrap_used,
+	unused_qualifications,
+	rust_2018_idioms,
+	clippy::expect_used,
+	trivial_casts,
+	trivial_numeric_casts,
+	unused_allocation,
+	clippy::as_conversions,
+	clippy::dbg_macro
+)]
+#![forbid(unsafe_code)]
+#![allow(clippy::module_name_repetitions)]
+
